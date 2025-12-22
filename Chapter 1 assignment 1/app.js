21a8c5a897f21ef29b99fe330c569c71230f89d0
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-22 10:56:10 +0500

    JS

---
+++ Chapter 1 assignment 1/app.js
@@ -0,0 +1,13 @@
+alert("Javascript Alert Box");
+
+alert("JS also known as JavaScript")
+
+alert("Hello! I am front-end developer as well as geographer")
+
+alert("Welcome in Front-End Developing world \n Happy coding coders!")
+
+alert("Welcome to JS land")
+
+alert("Happy coding \n prevent this page from creating additional dialogs")
+
+alert("Hello... I can run JS through my web browser's console")
