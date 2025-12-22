2dcce66563ca19d6c3f087bccfcb681b3b33e3df
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-22 11:58:03 +0500

    JS

--- Chapter 1 assignment 1/app.js
+++ Chapter 1 assignment 1/app.js
@@ -11,3 +11,5 @@ alert("Welcome to JS land")
 alert("Happy coding \n prevent this page from creating additional dialogs")
 
 alert("Hello... I can run JS through my web browser's console")
+
+var firstname = "Ufaq";
---
+++ Chapter 2 assignment 1/app.js
@@ -0,0 +1,19 @@
+let message;
+message = "Hello World";
+alert(message);
+
+let studentName;
+studentName = "Ufaq Fatima";
+alert(studentName);
+
+let age;
+age = "20 years old";
+alert(age);
+
+let pizza;
+pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+let email = "example@example.com";
+alert("My email address is " + email);
+
