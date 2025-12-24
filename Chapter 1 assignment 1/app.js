b555b8dd3b2e483fd606c37e79561525848fc6eb
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-25 01:46:42 +0500

    JS

--- Chapter 1 assignment 1/app.js
+++ Chapter 1 assignment 1/app.js
@@ -1,15 +1,10 @@
-alert("Javascript Alert Box");
 
-alert("JS also known as JavaScript")
+alert("Error! Please enter a valid password.");
 
-alert("Hello! I am front-end developer as well as geographer")
+alert("Welcome to JS land ...\n\Happy coding...");
 
-alert("Welcome in Front-End Developing world \n Happy coding coders!")
+alert("Welcome to JS land ...");
 
-alert("Welcome to JS land")
-
-alert("Happy coding \n prevent this page from creating additional dialogs")
-
-alert("Hello... I can run JS through my web browser's console")
-
-var firstname = "Ufaq";
+alert("Happy Coding");
+ 
+alert("Hello... I can run JS through my web browser's console");
--- Chapter 2 assignment 1/app.js
+++ Chapter 2 assignment 1/app.js
@@ -1,19 +1,75 @@
-let message;
-message = "Hello World";
-alert(message);
+// let message;
+// message = "Hello World";
+// alert(message);
 
-let studentName;
-studentName = "Ufaq Fatima";
-alert(studentName);
+// let studentName;
+// studentName = "Ufaq Fatima";
+// alert(studentName);
 
-let age;
-age = "20 years old";
-alert(age);
+// let age;
+// age = "20 years old";
+// alert(age);
 
-let pizza;
-pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-alert(pizza);
+// let pizza;
+// pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
 
-let email = "example@example.com";
-alert("My email address is " + email);
+// let email = "example@example.com";
+// alert("My email address is " + email);
 
+// let book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+// let symbols = "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬";
+// alert("ஜ۩۞۩ஜ" + symbols);
+
+// var message;
+// message = "hello world"
+// alert(message);
+
+// var firstName = "ufaq"
+// console.log(firstName);
+// console.log("firstName")
+
+// alert(firstName)
+
+// alert("ufaq\nfatima")
+// window.alert("hellowworld")
+// alert(s)
+
+var firstName = "ufaq"
+// console.log(firstName);
+// console.log("firstName")
+
+alert(firstName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert( "Umra \n Badar" );
+// window.alert("Hello world")
+// alert(5)
+
+// var firstName = "657"
+// console.log(firstName); 
+// console.log("firstName")
+
+// alert(firstName)
+
+
+// firstName = "Umra \n badar"
+// console.log(firstName); 
+
+// document.write(firstName)
+// alert(firstName)
+// var lastName;
