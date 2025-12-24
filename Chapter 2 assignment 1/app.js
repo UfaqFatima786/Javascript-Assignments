ec65ea8b8cc3cc1b1e49f64fe136e21021fef92f
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-25 02:38:04 +0500

    JS

--- Chapter 2 assignment 1/app.js
+++ Chapter 2 assignment 1/app.js
@@ -1,75 +1,53 @@
-// let message;
-// message = "Hello World";
-// alert(message);
 
-// let studentName;
-// studentName = "Ufaq Fatima";
-// alert(studentName);
+// 1. Declare a variable called username.
 
-// let age;
-// age = "20 years old";
-// alert(age);
+var userName = "Ufaq";
 
-// let pizza;
-// pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
-// alert(pizza);
-
-// let email = "example@example.com";
-// alert("My email address is " + email);
-
-// let book = "A smarter way to learn JavaScript";
-// alert("I am trying to learn from the book " + book);
-
-// let symbols = "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬";
-// alert("ஜ۩۞۩ஜ" + symbols);
-
-// var message;
-// message = "hello world"
-// alert(message);
-
-// var firstName = "ufaq"
-// console.log(firstName);
-// console.log("firstName")
-
-// alert(firstName)
-
-// alert("ufaq\nfatima")
-// window.alert("hellowworld")
-// alert(s)
-
-var firstName = "ufaq"
-// console.log(firstName);
-// console.log("firstName")
-
-alert(firstName)
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+var myName = "Ufaq Fatima";
 
 
 
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+var message 
+
+// b) Assign “Hello World” to variable message
+= "Hello World";
+
+// c) Display the message in alert box.
+alert(message);
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var studentName = "Ufaq Fatima";
+var studentAge = "20 years old";
+var studentCourse = "Modern Mobile Web and App Designing";
+alert(studentName)
+alert(studentAge)
+alert(studentCourse)
+
+// 5. Write a script to display the following alert using one JS variable:
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+var email="u.capricorn19@gmail.com";
+alert("My email address is " + email);
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following  message in an alert box:
+
+var book = "A smarter way to learn JavaScript";
+alert ("I am trying to learn from the book " + book);
+
+// 8. Write a script to display this in browser through JS
+document.write("Yah! I can write HTML content through JavaScript");
 
 
+// 9. Store following string in a variable and show in alert and  browser through JS “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”
 
-
-
-
-
-
-
-
-
-// alert( "Umra \n Badar" );
-// window.alert("Hello world")
-// alert(5)
-
-// var firstName = "657"
-// console.log(firstName); 
-// console.log("firstName")
-
-// alert(firstName)
-
-
-// firstName = "Umra \n badar"
-// console.log(firstName); 
-
-// document.write(firstName)
-// alert(firstName)
-// var lastName;
+var symbol = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert("symbol");
