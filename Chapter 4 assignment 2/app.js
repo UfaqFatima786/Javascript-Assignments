66d01a064fce8b69c6a38f1e396ebf649e37ae96
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-26 01:03:04 +0500

    JS

---
+++ Chapter 4 assignment 2/app.js
@@ -0,0 +1,33 @@
+alert("ufaq")
+// Valid Variables Names;
+var Ufaq;
+var myage;
+var _marks;
+var $price;
+var firstName;
+
+// Invalid Variables Names;
+
+// var -myage;
+// var 2ndname;
+// var my age;
+// var @name;
+// var first-name;
+
+// a) A heading stating “Rules for naming JS variables”
+var heading =(" Rules for naming JS variables");
+document.write("<h1>" + heading + "</h1>");
+
+
+// Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______. For example $my_1stVariable.
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name.
+// d) Variable names are case _________
+// e) Variable names should not be JS _________.
+
+document.write("Variable name can only contain numbers, $ and _. For example $my_1stVarible . <br>");
+document.write("Variable must begin with a letter, $ or _. For example $name, _name or name . <br>");
+document.write("Variable names are case sensitive . <br>");
+document.write("Variable names should not be JS keywords . <br>");
