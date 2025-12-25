d410a53945a24704041723be497830952247bf6b
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-26 00:16:52 +0500

    JS

--- Chapter 3 assignment 2/app.js
+++ Chapter 3 assignment 2/app.js
@@ -1,3 +1,4 @@
+// Declare a variable called age & assign to it your age. Show your age in an alert box.
 var age = 20;
 alert("I am " + age + " years old.");
 
@@ -6,3 +7,34 @@ alert("I am " + age + " years old.");
 var visitCount = 14;
 visitCount++;
 alert("You have visited this site " + visitCount + " times.");
+
+
+// Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+var birthyear = 2004;
+document.write("My birth year is " + birthyear + "<br>" + "Data type of my declared variable is number ." );
+
+// A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+
+
+
+
+var store = "XYZ Clothing store";
+var visitorname = "Ufaq Fatima";
+var Producttitle = "Long Froks";
+var Quantity = "ordered 5 ";
+document.write(visitorname + " " + Quantity + Producttitle + " " + "on" + " " + store);
+
+// alert("ufaq fatima ordered 5 long froks on XYZ Clothing store");
+
+// var visitorname = "Ufaq Fatima";
+// console.log(visitorname);
+
+ var salary = 100000
+console.log( "Your salary is "+ salary);
