68d2bc2f37c09bde988682aad0b2e5c3accab924
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-25 23:29:47 +0500

    js

---
+++ Chapter 3 assignment 2/app.js
@@ -0,0 +1,8 @@
+var age = 20;
+alert("I am " + age + " years old.");
+
+
+// Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You  have visited this site N times”.
+var visitCount = 14;
+visitCount++;
+alert("You have visited this site " + visitCount + " times.");
