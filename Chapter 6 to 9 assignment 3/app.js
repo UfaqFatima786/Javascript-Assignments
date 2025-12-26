03096f9ae18db8e8de0e9a89d902466f8557ded9
UfaqFatima786 <u.capricorn19@gmail.com>
2025-12-26 11:57:03 +0500

    JS

--- Chapter 6 to 9 assignment 3/app.js
+++ Chapter 6 to 9 assignment 3/app.js
@@ -1,1 +1,22 @@
-alert("ufaq")
+alert("ufaq");
+
+
+document.write("Result:<br>");
+  
+
+var num = 10;
+document.write("The value of num is: " + num + "<br>");
+document.write("...................................<br>");
+
+document.write("The value of ++num is: " + ++num + "<br>");
+document.write("Now the value of num is: " + num + "<br><br>");
+
+document.write("The value of num++ is: " + num++ + "<br>");
+document.write("Now the value of num is: " + num + "<br><br>");
+
+document.write("The value of --num is: " + --num + "<br>");
+document.write("Now the value of num is: " + num + "<br><br>");
+
+document.write("The value of num-- is: " + num-- + "<br>");
+document.write("Now the value of num is: " + num + "<br>");
+
